6882df17cc1c69dfb8a7287adad37f48082a08ef
julian <sjuliancarrizo@gmail.com>
2023-01-30 10:39:48 -0300

    Add publish section. Only send message to the last subscriber

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define v7_adapt
 
 //---------------
-// Thing to put in a separated file
+// Things to put in a separated file
 
 #define LIST_ADD_HEAD(type_, head_, elem_) \
   do {                                     \
@@ -38,6 +38,38 @@ uint16_t mg_ntohs(uint16_t net) {
 
 #define mg_htons(x) mg_ntohs(x)
 
+bool mg_match(struct mg_str s, struct mg_str p, struct mg_str *caps) {
+  size_t i = 0, j = 0, ni = 0, nj = 0;
+  if (caps) caps->p = NULL, caps->len = 0;
+  while (i < p.len || j < s.len) {
+    if (i < p.len && j < s.len && (p.p[i] == '?' || s.p[j] == p.p[i])) {
+      if (caps == NULL) {
+      } else if (p.p[i] == '?') {
+        caps->p = &s.p[j], caps->len = 1;     // Finalize `?` cap
+        caps++, caps->p = NULL, caps->len = 0;  // Init next cap
+      } else if (caps->p != NULL && caps->len == 0) {
+        caps->len = (size_t) (&s.p[j] - caps->p);  // Finalize current cap
+        caps++, caps->len = 0, caps->p = NULL;       // Init next cap
+      }
+      i++, j++;
+    } else if (i < p.len && (p.p[i] == '*' || p.p[i] == '#')) {
+      if (caps && !caps->p) caps->len = 0, caps->p = &s.p[j];  // Init cap
+      ni = i++, nj = j + 1;
+    } else if (nj > 0 && nj <= s.len && (p.p[ni] == '#' || s.p[j] != '/')) {
+      i = ni, j = nj;
+      if (caps && caps->p == NULL && caps->len == 0) {
+        caps--, caps->len = 0;  // Restart previous cap
+      }
+    } else {
+      return false;
+    }
+  }
+  if (caps && caps->p && caps->len == 0) {
+    caps->len = (size_t) (&s.p[j] - caps->p);
+  }
+  return true;
+}
+
 //-----------------
 
 const char * ssid = "South Africa 2.4G";
@@ -66,12 +98,12 @@ static void signal_handler(int signo) {
 static size_t mg_mqtt_next_topic(struct mg_mqtt_message *msg,
                                  struct mg_str *topic, uint8_t *qos,
                                  size_t pos) {
-  unsigned char *buf = (unsigned char *) msg->payload.ptr + pos;
+  unsigned char *buf = (unsigned char *) msg->payload.p + pos;
   size_t new_pos;
   if (pos >= msg->payload.len) return 0;
 
   topic->len = (size_t) (((unsigned) buf[0]) << 8 | buf[1]);
-  topic->ptr = (char *) buf + 2;
+  topic->p = (char *) buf + 2;
   new_pos = pos + 2 + topic->len + (qos == NULL ? 0 : 1);
   if ((size_t) new_pos > msg->payload.len) return 0;
   if (qos != NULL) *qos = buf[2 + topic->len];
@@ -101,8 +133,8 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
         // Client connects
         if (mm->payload.len < 9) {
           mg_error(c, "Malformed MQTT frame");
-        } else if (mm->payload.ptr[8] != 4) {
-          mg_error(c, "Unsupported MQTT version %d", mm->payload.ptr[8]);
+        } else if (mm->payload.p[8] != 4) {
+          mg_error(c, "Unsupported MQTT version %d", mm->payload.p[8]);
         } else {
           uint8_t response[] = {0, 0};
           mg_mqtt_send_header(c, MQTT_CMD_CONNACK, 0, sizeof(response));
@@ -124,10 +156,10 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
           sub->qos = qos;
           LIST_ADD_HEAD(struct sub, &s_subs, sub);
           MG_INFO(
-              ("SUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.ptr));
+              ("SUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.p));
           // Change '+' to '*' for topic matching using mg_match
           for (size_t i = 0; i < sub->topic.len; i++) {
-            if (sub->topic.ptr[i] == '+') ((char *) sub->topic.ptr)[i] = '*';
+            if (sub->topic.p[i] == '+') ((char *) sub->topic.p)[i] = '*';
           }
           resp[num_topics++] = qos;
         }
@@ -140,7 +172,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
       case MQTT_CMD_PUBLISH: {
         // Client published message. Push to all subscribed channels
         MG_INFO(("PUB %p [%.*s] -> [%.*s]", c->fd, (int) mm->data.len,
-                 mm->data.ptr, (int) mm->topic.len, mm->topic.ptr));
+                 mm->data.p, (int) mm->topic.len, mm->topic.p));
         for (struct sub *sub = s_subs; sub != NULL; sub = sub->next) {
           if (mg_match(mm->topic, sub->topic, NULL)) {
             mg_mqtt_pub(sub->c, mm->topic, mm->data, 1, false);
@@ -162,7 +194,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
     for (struct sub *next, *sub = s_subs; sub != NULL; sub = next) {
       next = sub->next;
       if (c != sub->c) continue;
-      MG_INFO(("UNSUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.ptr));
+      MG_INFO(("UNSUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.p));
       LIST_DELETE(struct sub, &s_subs, sub);
     }
   }
@@ -338,12 +370,29 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data)
         }
         case MG_EV_MQTT_PUBLISH:
         {
-          Serial.println("Publish");
-        }
+          for (struct sub *sub = s_subs; sub != NULL; sub = sub->next) 
+          {
+            if (mg_match(mm->topic, sub->topic, NULL)) 
+            {
+              char buf[100], *p = buf;
+              mg_asprintf(&p, sizeof(buf), "%.*s", (int) mm->topic.len, mm->topic.p);
+              if (p == NULL) 
+              {
+                return;
+              }                
+              mg_mqtt_publish(sub->c, p, mm->message_id, 0, mm->payload.p, mm->payload.len);
+              if (p != buf) 
+              {
+                free(p);
+              }
+            }
+          break;
+          }
         break;
 
-      }
+        }
 
+      }
     }
     else if (ev == MG_EV_ACCEPT)
     {
@@ -366,8 +415,8 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data)
         // Client connects
         if (mm->payload.len < 9) {
           //mg_error(c, "Malformed MQTT frame");
-        } else if (mm->payload.ptr[8] != 4) {
-          //mg_error(c, "Unsupported MQTT version %d", mm->payload.ptr[8]);
+        } else if (mm->payload.p[8] != 4) {
+          //mg_error(c, "Unsupported MQTT version %d", mm->payload.p[8]);
         } else {
           uint8_t response[] = {0, 0};
           mg_mqtt_send_header(c, MQTT_CMD_CONNACK, 0, sizeof(response));
@@ -388,10 +437,10 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data)
           sub->topic = mg_strdup(topic);
           sub->qos = qos;
           LIST_ADD_HEAD(struct sub, &s_subs, sub);
-          //MG_INFO(("SUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.ptr));
+          //MG_INFO(("SUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.p));
           // Change '+' to '*' for topic matching using mg_match
           for (size_t i = 0; i < sub->topic.len; i++) {
-            if (sub->topic.ptr[i] == '+') ((char *) sub->topic.ptr)[i] = '*';
+            if (sub->topic.p[i] == '+') ((char *) sub->topic.p)[i] = '*';
           }
           resp[num_topics++] = qos;
         }
@@ -404,7 +453,7 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data)
       case MQTT_CMD_PUBLISH: {
         // Client published message. Push to all subscribed channels
         //MG_INFO(("PUB %p [%.*s] -> [%.*s]", c->fd, (int) mm->data.len,
-        //         mm->data.ptr, (int) mm->topic.len, mm->topic.ptr));
+        //         mm->data.p, (int) mm->topic.len, mm->topic.p));
         for (struct sub *sub = s_subs; sub != NULL; sub = sub->next) {
           if (mg_match(mm->topic, sub->topic, NULL)) {
             mg_mqtt_pub(sub->c, mm->topic, mm->data, 1, false);
@@ -426,7 +475,7 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data)
     for (struct sub *next, *sub = s_subs; sub != NULL; sub = next) {
       next = sub->next;
       if (c != sub->c) continue;
-      //MG_INFO(("UNSUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.ptr));
+      //MG_INFO(("UNSUB %p [%.*s]", c->fd, (int) sub->topic.len, sub->topic.p));
       LIST_DELETE(struct sub, &s_subs, sub);
     }
   }
